c08c1dc9d59048783652dd0b97eb479bd6e80f3b
aegooby <aegooby@gmail.com>
2022-11-25 22:02:17 -0500

    Penis..
    Co-Authored-By: Nika <67396590+tostig77@users.noreply.github.com>

--- src/codec.rs
+++ src/codec.rs
@@ -1,17 +1,23 @@
 use anyhow::Error;
-use tokio_util::codec::Decoder;
+use tokio_util::codec::{Decoder, Encoder};
 
-#[derive(Debug)]
-pub struct Message;
+use crate::proto;
 
 pub struct Codec;
 
 impl Decoder for Codec {
-    type Item = Message;
-
+    type Item = proto::ping::Ping;
     type Error = Error;
 
     fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        todo!()
+    }
+}
+
+impl<Item> Encoder<Item> for Codec {
+    type Error = Error;
+
+    fn encode(&mut self, item: Item, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
         todo!()
     }
 }
