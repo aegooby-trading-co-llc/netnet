53702546b4fb543b4d9dc4682bedde80ba370ed7
aegooby <aegooby@gmail.com>
2022-12-17 23:12:16 -0500

    Penis
    Co-Authored-By: Nika <67396590+tostig77@users.noreply.github.com>

--- ci/sccache.sh
+++ ci/sccache.sh
@@ -1,4 +1,4 @@
-#!/usr/env/bin bash
+#!/bin/bash
 
 curl --output sccache.tar.gz --location https://github.com/mozilla/sccache/releases/download/v0.3.1/sccache-v0.3.1-x86_64-unknown-linux-musl.tar.gz; 
 tar -xvf sccache.tar.gz; 
