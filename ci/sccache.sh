430d6825158bf01afde8f76bcd3557bb10db652f
aegooby <aegooby@gmail.com>
2022-12-23 18:25:10 -0500

    Beeeenis

--- ci/sccache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl --output sccache.tar.gz --location https://github.com/mozilla/sccache/releases/download/v0.3.1/sccache-v0.3.1-x86_64-unknown-linux-musl.tar.gz; 
-tar -xvf sccache.tar.gz; 
-cp sccache-v0.3.1-x86_64-unknown-linux-musl/sccache /usr/local/bin/sccache;
-chmod u+x /usr/local/bin/sccache
